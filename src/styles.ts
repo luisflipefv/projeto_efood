907fdc4d791309bff06a30425e51cb0a2bdd64b4
Luis Felipe Franca <luisfelipefv8@hotmail.com>
2024-06-12 08:16:59 -0300

    commit inicial

---
+++ src/styles.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const cores = {
+  bege1: "#fff8f2",
+  bege2: "#ffebd9",
+  rosa: "#e66767",
+  branco: "#ffffff",
+};
+
+export const GlobalCss = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif
+  }
+  body{
+  background-color: ${cores.bege2};
+  color: ${cores.rosa};
+  }
+`;
